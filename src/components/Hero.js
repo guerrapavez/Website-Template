3c86203de34a9506c2e185e5388dafe8ee5aeb7d
Reynel Guerra <guerrapa@ualberta.ca>
2024-01-14 19:43:41 -0700

    center description

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
                 </div>
                 <div className="m-auto overflow-hidden mx-4 mt-8 lg:mt-4 p-2 md:p-12 h-5/6" data-aos="zoom-in">
 
-                    <div className="flex flex-col lg:flex-row py-8 justify-between text-center lg:text-left">
+                    <div className="flex flex-col py-8 justify-between text-center">
                         <div className="flex flex-col justify-center" data-aos="zoom-in" data-aos-delay="200">
                         <h1 className="mb-5 md:text-5xl text-3xl font-bold" style={{ color: colors.haseBlue }}>
                             The Next Level to Your Success
